f1b074589e03eba11ac052270ebad5da47669904
Gabriel Adao <gabriel.adao@zup.com.br>
2020-07-17 16:01:54 -0300

    First version Local WebSocket - Beagle Chat

---
+++ backend/beagle-chat/src/main/kotlin/br/com/zup/beagle/chat/config/WebSocketConfig.kt
@@ -0,0 +1,30 @@
+package br.com.zup.beagle.chat.webSocket.config
+
+import org.springframework.context.annotation.Configuration
+import org.springframework.messaging.simp.config.MessageBrokerRegistry
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer
+
+@Configuration
+@EnableWebSocketMessageBroker
+class WebSocketConfig : WebSocketMessageBrokerConfigurer {
+    override fun registerStompEndpoints(registry: StompEndpointRegistry) {
+        registry.addEndpoint("/websocket").withSockJS()
+    }
+
+    //    USANDO O RABBITMQ
+    override fun configureMessageBroker(registry: MessageBrokerRegistry) {
+        registry.setApplicationDestinationPrefixes("/app")
+        registry.enableStompBrokerRelay("/topic").setRelayHost("localhost")
+                .setRelayPort(61613)
+                .setClientLogin("guest")
+                .setClientPasscode("guest")
+    }
+
+    //    NÃO USANDO O RABBITMQ
+   /* override fun configureMessageBroker(registry: MessageBrokerRegistry) {
+        registry.setApplicationDestinationPrefixes("/app")
+        registry.enableSimpleBroker("/topic");
+    }*/
+}
