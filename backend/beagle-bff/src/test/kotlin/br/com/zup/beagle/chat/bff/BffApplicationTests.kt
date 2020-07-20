6316a07b794caf061855eeafcda092be6a3a4c36
tereza.niederauer <tereza.niederauer@zup.com.br>
2020-07-20 12:06:53 -0300

    adjustment in the directories structure to function as modules

---
+++ backend/beagle-bff/src/test/kotlin/br/com/zup/beagle/chat/bff/BffApplicationTests.kt
@@ -0,0 +1,13 @@
+package br.com.zup.beagle.chat.bff
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class BffApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
