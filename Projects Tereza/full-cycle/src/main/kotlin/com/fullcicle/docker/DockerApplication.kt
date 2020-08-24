8195ecf4499dd163f0bd5a6f6769ecf87356f3c9
Tereza Niederauer <tereza.niederauer@zup.com.br>
2020-08-24 20:59:37 -0300

    init commit

---
+++ Projects Tereza/full-cycle/src/main/kotlin/com/fullcicle/docker/DockerApplication.kt
@@ -0,0 +1,11 @@
+package com.fullcicle.docker
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class DockerApplication
+
+fun main(args: Array<String>) {
+runApplication<DockerApplication>()
+}
